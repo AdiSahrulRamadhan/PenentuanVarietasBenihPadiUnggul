af80ce985c27ca8895913b152106d84fd83a42ba
Adi Sahrul Ramadhan <160815677+AdiSahrulRamadhan@users.noreply.github.com>
2026-06-18 12:38:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1321,7 +1321,7 @@ elif selected == "Pembobotan":
         - n adalah jumlah total kriteria yang dibandingkan dalam matriks perbandingan berpasangan AHP, yang digunakan untuk menghitung CI dan CR.
         """)
         st.markdown("**Penjelasan & Tujuan:** Mengukur tingkat konsistensi")
-        CI = (lambda_max - n) / (n - 1)
+        CI = (lambda_max - n*2) / (n*2 - 1)
         st.markdown("**Perhitungan CI:**")
         st.markdown(f"- λ_max (rata-rata λ_i) = {lambda_max:.6f}")
         st.markdown(f"- n (jumlah kriteria) = {n}")
