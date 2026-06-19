5399803c0a236281059054127e70c5ab9bf451dd
Adi Sahrul Ramadhan <160815677+AdiSahrulRamadhan@users.noreply.github.com>
2026-06-19 09:03:40 +0700

    tes saja

--- app.py
+++ app.py
@@ -943,7 +943,7 @@ elif selected == "Pembobotan":
     else:
         kriteria = [col for col in df.columns if col != "Alternatif"]
         kriteria = sorted(kriteria, key=lambda x: int(x.replace("C","")))
-        n = len(kriteria)
+        n = len(kriteria) *2
         config_db = load_db("config") or {}
         bobot_db = load_db("bobot") or {}
         if "ahp_matrix" in config_db and "ahp_matrix" not in st.session_state:
