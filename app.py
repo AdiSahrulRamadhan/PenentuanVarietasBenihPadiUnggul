2efc81160b8238e8cda0b00438dd8508f4249ff2
Adi Sahrul Ramadhan <160815677+AdiSahrulRamadhan@users.noreply.github.com>
2026-07-02 17:32:42 +0700

    fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 from streamlit_option_menu import option_menu
 from io import BytesIO
 import os
-# DATABASE
+# DATABASE 
 DB_PATH = "database.db"
 def get_conn():
     return sqlite3.connect(DB_PATH, check_same_thread=False)
